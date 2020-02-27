59e43929403596ad11b8403d40685b7fb4a3c25b
Filip Grudzien <filip.grudzien@capgemini.com>
2020-02-27 11:07:36 +0100

    updated side effects handling with error messages and navigation

---
+++ src/app/shared/components/error-bar/error-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-error-bar',
+  templateUrl: './error-bar.component.html',
+  styleUrls: ['./error-bar.component.css']
+})
+export class ErrorBarComponent implements OnInit {
+
+  @Input() errorMessage: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
